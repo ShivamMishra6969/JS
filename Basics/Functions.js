de61cbe19cd513602e1842bff8050561aca635aa
shivam mishra <sm20160168284@outlook.com>
2025-08-12 16:19:48 +0530

    functions

--- Basics/Functions.js
+++ Basics/Functions.js
@@ -1,3 +1,17 @@
 console.log("functions in javascript");
 
+const prompt=require("prompt-sync")();
+
+//* Function to find the table of a number
+function table(num){
+    for(let i=1;i<=10;i++){
+        console.log(num,"x",i,"=",num*i);
+    }
+}
+
+let num=prompt("Enter a number to find its table = ");
+num=Number.parseInt(num);
+
+table(num)
+
 
