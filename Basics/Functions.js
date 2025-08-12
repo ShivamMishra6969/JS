963a67d38062af543ec7889f59ca5dac397e026b
shivam mishra <sm20160168284@outlook.com>
2025-08-12 16:37:30 +0530

    functions

--- Basics/Functions.js
+++ Basics/Functions.js
@@ -3,15 +3,22 @@ console.log("functions in javascript");
 const prompt=require("prompt-sync")();
 
 //* Function to find the table of a number
-function table(num){
-    for(let i=1;i<=10;i++){
-        console.log(num,"x",i,"=",num*i);
-    }
+// function table(num){
+//     for(let i=1;i<=10;i++){
+//         console.log(num,"x",i,"=",num*i);
+//     }
+// }
+
+// let num=prompt("Enter a number to find its table = ");
+// num=Number.parseInt(num);
+
+// table(num);
+
+//* Arrow functions
+
+const greet=(name)=>{
+    console.log(`Greetings from ${name}`);
 }
+let name=prompt("Enter agent name=");
 
-let num=prompt("Enter a number to find its table = ");
-num=Number.parseInt(num);
-
-table(num);
-
-
+greet(name);
