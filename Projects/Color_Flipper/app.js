e702e124c7d7801ac5f5be7a7d3ee0c64981fbee
shivam mishra <sm20160168284@outlook.com>
2025-08-13 13:28:02 +0530

    projects

--- Projects/Color_Flipper/app.js
+++ Projects/Color_Flipper/app.js
@@ -1,1 +1,16 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red","orange","violet", "rgba(133,122,200)", "#25b0f1ff"];
+
+
+const btn=document.querySelector("#btn");
+const color=document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+    const randomNum=getRandomNum();
+    document.body.style.backgroundColor=colors[randomNum];
+    color.textContent=colors[randomNum]
+    
+})
+
+const getRandomNum=()=>{
+    return Math.floor(Math.random() * colors.length);
+}
--- Projects/Color_Flipper/hex.js
+++ Projects/Color_Flipper/hex.js
@@ -1,1 +1,18 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+
+const btn=document.getElementById("btn");
+const color=document.querySelector(".color");
+
+btn.addEventListener("click",()=>{
+    let hexCode= "#";
+    for(let i=0;i<6;i++){
+        hexCode +=hex[getRandomHex()];
+    }
+    color.textContent=hexCode;
+    document.body.style.backgroundColor=hexCode;
+})
+
+function getRandomHex(){
+    return Math.floor(Math.random()*hex.length)
+}
